093d6fa99e1d611690ff5e45ece21423375a2c53
bobetbat <b.bobetayev.97@gmail.com>
2023-03-13 18:48:59 +0100

    feat: added contract scripts

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,7 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+//TODO: Add more networks
+//TODO: Add network RPC URLs to the .env file
+//TODO: Add private key to the .env file
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+      chainId: 5,
+    }
+  },
   solidity: "0.8.17",
 };
 
