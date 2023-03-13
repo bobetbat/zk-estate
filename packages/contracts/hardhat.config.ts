22437de69eb0219e8d9f7028e1c6126b461c879d
bobetbat <b.bobetayev.97@gmail.com>
2023-03-13 19:52:26 +0100

    feat: added hardhat networks config

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -7,10 +7,40 @@ import "@nomicfoundation/hardhat-toolbox";
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
-    goerli: {
-      url: process.env.GOERLI_RPC_URL,
+    goerliTestnet: {
+      url: process.env.GOERLI_TESTNET_RPC_URL,
       accounts: [process.env.PRIVATE_KEY ?? ""],
       chainId: 5,
+    },
+    polygonZkEvmTestnet: {
+      url: process.env.POLYGON_ZKEVM_TESTNET_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+      chainId: 1442,
+    },
+    scrollTestnet: {
+      url: process.env.SCROLL_ALFA_TESTNET_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+      chainId: 534353,
+    },
+    gnosis: {
+      url: process.env.GNOSIS_MAINNET_RPS_URL,
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+      chainId: 100,
+    },
+    gnosisTestnet: {
+      url: process.env.GNOSIS_CHIADO_TESTNET_RPS_URL,
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+      chainId: 10200,
+    },
+    optimismTestnet: {
+      url: process.env.OP_GOERLI_TESTNET_PRC_UTL,
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+      chainId: 420,
+    },
+    optimism: {
+      url: process.env.OP_MAINNET_PRC_UTL,
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+      chainId: 10,
     }
   },
   solidity: "0.8.17",
